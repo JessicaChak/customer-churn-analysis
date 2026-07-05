f899cb400997bf737a415432cf2b505668d683a0
JessicaChak <ctkjessica@gmail.com>
2026-07-06 06:33:26 +0800

    Add revenue at risk query
    Added SQL query to estimate revenue at risk from churned customers.

--- sql/q5_revenue_at_risk.sql
+++ sql/q5_revenue_at_risk.sql
@@ -1,1 +1,6 @@
 
+-- Q5: Estimate Revenue at Risk from Churned Customers
+
+SELECT	ROUND (SUM(CASE WHEN "Churn Label" = 'Yes' THEN "Monthly Charges" ELSE 0 END), 2) AS revenue_at_risk
+    FROM customers
+  
